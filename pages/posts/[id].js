3fc222a17e40597e6e693cef34ca1ead07bd1588
kakegawa167 <kakesho167@gmail.com>
2023-12-29 15:03:45 +0900

    all done

---
+++ pages/posts/[id].js
@@ -0,0 +1,42 @@
+import Layout from "@/components/Layout";
+import { getAllPostsIDs, getEachPostData } from "@/lib/post";
+import utilStyle from "../../styles/utils.module.css";
+import Head from "next/head";
+
+export async function getStaticPaths(){
+    const paths = getAllPostsIDs();
+
+    return {
+        paths,
+        fallback: false,
+    }
+}
+
+export async function getStaticProps({ params }){
+    const postData = await getEachPostData(params.id);
+
+    return {
+        props: {
+            postData
+        }
+    }
+}
+
+export default function Post( {postData}) {
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyle.headingX1}>
+                    {postData.title}
+                </h1>                
+                <div className={utilStyle.lightText}>
+                    {postData.date}
+                </div>
+                <div dangerouslySetInnerHTML={{__html: postData.blogContentHTML}} />
+            </article>
+        </Layout>
+    );
+}
